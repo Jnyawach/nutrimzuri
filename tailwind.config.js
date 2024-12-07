a133712a85fff4e9d972741bb95d23fd41e23e73
Jnyawach <nyawach41@gmail.com>
2024-12-07 16:33:14 +0300

    updated homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ export default {
         extend: {
             fontFamily: {
                 'matter': ['Matter', 'serif'],
+                'outfit': ['Outfit', 'sans-serif'],
             },
         },
     },
